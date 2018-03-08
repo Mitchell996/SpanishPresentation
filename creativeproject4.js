9f9b26a63772d7fc0077f2f44203741022ea5fd1
Mitchell996 <35315827+Mitchell996@users.noreply.github.com>
2018-03-08 10:04:11 -0700

    Last commit: hopefully

--- creativeproject4.js
+++ creativeproject4.js
@@ -1,51 +1,51 @@
- window.onload=function(){
+window.onload=function(){
       var quiz = {
-        title: 'Prueba de equivalentes de \“to  run\”',
+        title: 'Prueba de equivalentes de \"to  run\"',
         questions: [
           {
-            text: "1. Soy Mitt Romney y \" \" senador de Utah.  -I’m running for this office.",
+            text: "1. Soy Mitt Romney y \" \" senador de Utah.  -I'm running for this office.",
             responses: [
               {text: 'estoy corriendo por'}, 
               {text: 'aspiro a', correct: true}, 
             ]
           }, {
-            text: "2. La salsa es tan picante que su nariz \“he has a running nose\”.",
+            text: "2. La salsa es tan picante que su nariz \"he has a running nose\".",
             responses: [
               {text: 'gotear', correct: true}, 
               {text: 'correr'},
             ]
           }, {
-            text: "3. Ayer, \“I ran into my friend who I haven’t seen in a while\”" ,
+            text: "3. Ayer, \"I ran into my friend who I haven't seen in a while\"." ,
             responses: [
-              {text: 'me gotee con'},
-              {text: 'me tropezé con', correct: true},
+              {text: 'me goteé con'},
+              {text: 'me tropez&eacute; con', correct: true},
             ]
           }, {
-            text: "4. Solo Superman puede \“to run\”  rápidamente como un tren.",
+            text: "4. Solo Superman puede \"to run\" rápidamente como un tren.",
             responses: [
               {text: 'postularse'},
               {text: 'correr', correct: true},
             ]
           }, {
-            text: "5. Señora disculpe \“is your fridge running\”",
+            text: "5. Señora disculpe \"is your fridge running\"",
             responses: [
               {text: 'corriendo'}, 
               {text: 'funcionando', correct: true},
             ]
           }, {
-            text: "6. Mi amigo y yo \“are running an event\”",
+            text: "6. Mi amigo y yo \"are running an event\"",
             responses: [
               {text: 'dirigimos', correct: true}, 
               {text: 'tropezamos'},
             ]
           }, {
-            text: "7. Si cuando lavo la ropa blanca pongo una media, calcetín rojo entonces el color \“runs\”.",
+            text: "7. Si cuando lavo la ropa blanca pongo una media, calcetín rojo entonces el color \"runs\".",
             responses: [
               {text: 'se corre', correct: true},
               {text: 'se extiende'},
             ]
           }, {
-            text: "8. La liebre \“runs\” más rápido que la tortuga.",
+            text: "8. La liebre \"runs\"más rápido que la tortuga.",
             responses: [
               {text: 'corriendo'}, 
               {text: 'corre', correct: true},
