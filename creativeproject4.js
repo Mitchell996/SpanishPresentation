b3690814b3ece29c75c56172c2f93a392b07284d
Mitchell996 <35315827+Mitchell996@users.noreply.github.com>
2018-03-08 10:17:34 -0700

    Add files via upload

--- creativeproject4.js
+++ creativeproject4.js
@@ -1,9 +1,9 @@
-window.onload=function(){
-      var quiz = {
+window.onload=function(){ 
+    var quiz = {
         title: 'Prueba de equivalentes de \"to  run\"',
         questions: [
           {
-            text: "1. Soy Mitt Romney y \" \" senador de Utah.  -I'm running for this office.",
+            html: '1. Soy Mitt Romney y " " senador de Utah.  -Im running for this office.',
             responses: [
               {text: 'estoy corriendo por'}, 
               {text: 'aspiro a', correct: true}, 
@@ -17,17 +17,17 @@ window.onload=function(){
           }, {
             text: "3. Ayer, \"I ran into my friend who I haven't seen in a while\"." ,
             responses: [
-              {text: 'me goteé con'},
-              {text: 'me tropez&eacute; con', correct: true},
+              {text: 'me gotee\' con'},
+              {text: 'me tropeze\'; con', correct: true},
             ]
           }, {
-            text: "4. Solo Superman puede \"to run\" rápidamente como un tren.",
+            text: "4. Solo Superman puede \"to run\" rapidamente como un tren.",
             responses: [
               {text: 'postularse'},
               {text: 'correr', correct: true},
             ]
           }, {
-            text: "5. Señora disculpe \"is your fridge running\"",
+            text: "5. Senora disculpe \"is your fridge running\"",
             responses: [
               {text: 'corriendo'}, 
               {text: 'funcionando', correct: true},
@@ -45,13 +45,13 @@ window.onload=function(){
               {text: 'se extiende'},
             ]
           }, {
-            text: "8. La liebre \"runs\"más rápido que la tortuga.",
+            text: "8. La liebre \"runs\"mas rapido que la tortuga.",
             responses: [
               {text: 'corriendo'}, 
               {text: 'corre', correct: true},
             ]
           }, {
-            text: "9. Los límites de la cuidad \"run\" desde el río hasta las montañas.",
+            text: "9. Los limites de la cuidad \"run\" desde el rio hasta las montanas.",
             responses: [
               {text: 'corren'},
               {text: 'se extienden', correct: true},
